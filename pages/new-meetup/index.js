fd5c0b7dcffcd0ea7a8dab6c53df4678e2a51958
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-07-12 15:11:00 +0430

    Request the data to my API

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,26 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 const NewMeetup = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const res = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+
+    router.push("/");
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
+
+// export const
 
 export default NewMeetup;
